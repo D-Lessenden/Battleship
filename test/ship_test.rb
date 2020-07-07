d34470c20d95734a734d0a6691cbc14f999a0b93
Daniel Lessenden <dless27@gmail.com>
2020-07-06 20:12:04 -0500

    Add sunk and hit test

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -7,7 +7,6 @@ class ShipTest < Minitest::Test
 
   def setup
     @cruiser = Ship.new("Cruiser", 3)
-    #binding.pry
   end
 
   def test_ship_exists
@@ -17,19 +16,17 @@ class ShipTest < Minitest::Test
   def test_it_has_health
     @cruiser.health
     assert_equal 3, @cruiser.health
-    @cruiser.hit
-    @cruiser.hit
 
-    binding.pry
+    @cruiser.hit
     assert_equal 2, @cruiser.health
   end
 
   def test_has_it_sunk?
     assert_equal false, @cruiser.sunk?
+
+    @cruiser.hit
+    @cruiser.hit
+    @cruiser.hit
+    assert_equal true, @cruiser.sunk?
   end
-
-   # def test_has_been_hit
-   #   #assert_equal true,
-   # end
-
 end
