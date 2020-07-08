33c975630e5faf887d4f752db1e6b67d563a252b
Daniel Lessenden <dless27@gmail.com>
2020-07-08 00:16:55 -0500

    Create board_test and valid_coord_test

---
+++ test/board_test.rb
@@ -0,0 +1,36 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require 'pry'
+
+class BoardTest < Minitest::Test
+
+  def setup
+    @board = Board.new
+  end
+
+  def test_it_exists
+    assert_instance_of Board, @board
+  end
+
+  def test_valid_coord
+    @board.valid_coordinate?("A1")
+    assert true, @board.valid_coordinate?("A1")
+
+    @board.valid_coordinate?("D4")
+    assert true, @board.valid_coordinate?("D4")
+
+    @board.valid_coordinate?("A5")
+    refute @board.valid_coordinate?("A5")
+
+    @board.valid_coordinate?("E1")
+    assert_equal false, @board.valid_coordinate?("E1")
+    #Not sure if syntax on line 26 or line 29 is better
+
+    @board.valid_coordinate?("A22")
+    refute @board.valid_coordinate?("A22")
+  end
+
+end
