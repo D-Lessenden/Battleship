f68d22ac71d50e19b80cbe2a69e4ab09d4c648a5
PhilipDeFraties <philipjfry338@gmail.com>
2020-07-10 21:47:20 -0600

    Updated from Daniel's code, added overlap check to valid_placement?

--- lib/board.rb
+++ lib/board.rb
@@ -2,7 +2,7 @@ class Board
   attr_reader :cells #added attr_reader w/ :cells so that @board.cells responds as indicated in specs
 
   def initialize
-    cells = @cells
+    # cells = @cells
     @cells = {}
   end
 
@@ -31,12 +31,15 @@ class Board
     ord = coordinate_letters.map(&:ord)
     num = coordinate_numbers.map(&:to_i)
 
+
+    
     (ship.length == 2 && coordinates.length == 2 ||
     ship.length == 3 && coordinates.length == 3) &&
     (((coordinate_letters.uniq.size == 1 &&
     (num.each_cons(2).all? { |x,y| y == x + 1})) ||
-    (num.uniq.size == 1 && ord.each_cons(2).all? { |x,y| y == x + 1})))
-    #need to add a @cell empty? boolean
+    (num.uniq.size == 1 && ord.each_cons(2).all? { |x,y| y == x + 1}))) &&
+    (coordinates.all? {|cell| @cells[cell].empty?})
+    # binding.pry
   end
 
   def place(ship, coordinates)
@@ -46,4 +49,31 @@ class Board
       end
     end
   end
+
+  def render
+    # @boards.cells each do |cord|
+    #   cord.cell.render
+    #@cell.render #screen shot from 1 04 was set up like this
+
+   #  @board.cells.each do |k, v|
+   #   @board.cells[k].render
+   # end
+
+   a_row = @cells.keys.select{ |keys| keys[0][0] == "A"}
+   b_row = @cells.keys.select{ |keys| keys[0][0] == "B"}
+   c_row = @cells.keys.select{ |keys| keys[0][0] == "C"}
+   d_row = @cells.keys.select{ |keys| keys[0][0] == "D"}
+
+
+   puts " 1 2 3 4"
+   #for each cell (hash key or value) run thru render
+   p "A #{a_row.map { |key| @cells[key].render}.join(" ")} |"
+   p "B #{b_row.map { |key| @cells[key].render}.join(" ")} |"
+   p "C #{c_row.map { |key| @cells[key].render}.join(" ")} |"
+   p "D #{d_row.map { |key| @cells[key].render}.join(" ")} |"
+
+
+  end
+
+
 end #class
