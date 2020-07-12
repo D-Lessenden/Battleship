868b64ba87e43f7cac9be6ab46e15d0d7d0336ae
PhilipDeFraties <philipjfry338@gmail.com>
2020-07-12 12:32:54 -0600

    Changing setup method and tests to fix scope issues when working on @game.turn method.

--- test/game_test.rb
+++ test/game_test.rb
@@ -8,11 +8,29 @@ require 'pry'
 
 class GameTest < Minitest::Test
 
+def setup
+  @game = Game.new
+  @board = Board.new
+  @board.generate_cells
+  @cruiser = Ship.new("Cruiser", 3)
+  @submarine = Ship.new("Submarine", 2)
+end
 
   def test_it_exists
     game = Game.new
-    binding.pry
+
     assert_instance_of Game, @game
+  end
+
+def test_turn
+    # game = Game.new
+    # # @board = Board.new
+    # # @board.generate_cells
+    # cruiser = Ship.new("Cruiser", 3)
+    # submarine = Ship.new("Submarine", 2)
+    @board.place(@cruiser, ["A1", "A2", "A3"])
+    binding.pry
+    @game.turn
   end
 
 end
