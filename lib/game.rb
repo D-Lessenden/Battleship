1e78db6b4bc3b45baf61d7160e2e7d6889c62ed7
PhilipDeFraties <philipjfry338@gmail.com>
2020-07-12 15:14:26 -0600

    Changes to turn logic.

--- lib/game.rb
+++ lib/game.rb
@@ -1,43 +1,44 @@
 require 'pry'
 class Game
-
+  attr_reader :board
   def initialize
-    # @board = Board.new
-    # @board.generate_cells
-    # @cruiser = Ship.new("Cruiser", 3)
-    # @submarine = Ship.new("Submarine", 2)
+    @board = Board.new
+    @board.generate_cells
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
-   # def main_menu
-  #   puts "Welcome to BATTLESHIP\nEnter p to play. Enter q to quit"
-  #   @initial_input = gets.chomp!
-  #     if @initial_input == "p"
-  #       puts "I have laid out my ships on the grid.\n
-  #       You now need to lay out your two ships.\n
-  #       The Cruiser is three units long and the Submarine is two units long."
-  #       #sleep(?)
-  #       puts "  1 2 3 4\nA . . . .\nB . . . .\nC . . . .\nD . . . ."
-  #       puts "Enter the squares for the Cruiser (3 spaces):"
-  #       @user_cruiser_placement = gets.chomp.upcase!
-  #       @board.place(cruiser, @user_cruiser_placement)
-  #
-  #         puts "Those are invalid coordinates. Please try again."
-  #         puts "Enter the squares for the Sub (2 spaces):"
-  #       @user_sub_placement = gets.chomp.upcase!
-  #       if true @board.valid_placement?
-  #       elsif false
-  #         puts "Those are invalid coordinates. Please try again."
-  #       end
-  #
-  #       #ship_placements(method) should verify valid coordinate, transforms data to array, and places ship
-  #       @board.render(true)
-  #
-  #     elsif @initial_input == "q"
-  #       puts "Bye Felicia"
-  #     elsif @initial_input != "p" || "q"
-  #       puts "Enter p to play. Enter q to quit"
-  #     end
-   # end
+   def main_menu
+      @board.place(@cruiser, ["A1", "A2", "A3"])
+    # puts "Welcome to BATTLESHIP\nEnter p to play. Enter q to quit"
+    # @initial_input = gets.chomp!
+    #   if @initial_input == "p"
+    #     puts "I have laid out my ships on the grid.\n
+    #     You now need to lay out your two ships.\n
+    #     The Cruiser is three units long and the Submarine is two units long."
+    #     #sleep(?)
+    #     puts "  1 2 3 4\nA . . . .\nB . . . .\nC . . . .\nD . . . ."
+    #     puts "Enter the squares for the Cruiser (3 spaces):"
+    #     @user_cruiser_placement = gets.chomp.upcase!
+    #     @board.place(cruiser, @user_cruiser_placement)
+    #
+    #       puts "Those are invalid coordinates. Please try again."
+    #       puts "Enter the squares for the Sub (2 spaces):"
+    #     @user_sub_placement = gets.chomp.upcase!
+    #     if true @board.valid_placement?
+    #     elsif false
+    #       puts "Those are invalid coordinates. Please try again."
+    #     end
+    #
+    #     #ship_placements(method) should verify valid coordinate, transforms data to array, and places ship
+    #     @board.render(true)
+    #
+    #   elsif @initial_input == "q"
+    #     puts "Bye Felicia"
+    #   elsif @initial_input != "p" || "q"
+    #     puts "Enter p to play. Enter q to quit"
+    #   end
+   end
   #
   def game_board
     p "=============COMPUTER BOARD============="
@@ -58,17 +59,18 @@ class Game
       end
     @board.verify_and_fire(shot)
     #build helper method for computer fire -D
-      if false
-        "You already shot at this coordinate, check your board :)"
-      # elsif @board.cells[shot].empty? && (@board.cells[shot].misses == 1)
-      #   "Your shot missed"
-      # elsif @board.cells[shot].ship.sunk? == true && (@board.cells[shot].hit == 1)
-      #   "You hit and sunk a ship!"
-      else
-        "You hit a ship!"
-      end
+        # if @board.cells[shot].
+        #    "You already fired at this spot"
+        if @board.cells[shot].fired_upon? == true && @board.cells[shot].ship.sunk? == true
+           "Sunk"
+        elsif @board.cells[shot].fired_upon? == true && @board.cells[shot].ship.sunk? == false
+           "Hit"
+        elsif @board.cells[shot].empty? == true
+          "Miss"
+        end
     #If cell already fired upon, user notified
   end
+
 
   # def check_user_input(user_input)
   #   until @board.valid_coordinate?(shot) == true
