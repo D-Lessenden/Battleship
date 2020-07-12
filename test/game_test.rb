865b17d107fa82d81e14d7de7e48944373a8bcde
Daniel Lessenden <dless27@gmail.com>
2020-07-11 22:27:30 -0500

    Add test for cpu_place_ship methods

--- test/game_test.rb
+++ test/game_test.rb
@@ -8,11 +8,45 @@ require 'pry'
 
 class GameTest < Minitest::Test
 
+  def setup
+    @game = Game.new
+    @board = Board.new
+    @board.generate_cells
+  end
 
   def test_it_exists
-    game = Game.new
-    binding.pry
     assert_instance_of Game, @game
+  end
+
+  def test_cpu_place_cruiser
+    refute nil, @game.cpu_place_cruiser
+    refute nil, @game.cpu_place_sub
+  end
+  # def test_cpu_place_cruiser2
+  #   refute nil, @game.cpu_place_cruiser
+  # end
+  # def test_cpu_place_cruiser3
+  #   refute nil, @game.cpu_place_cruiser
+  # end
+  # def test_cpu_place_cruiser4
+  #   refute nil, @game.cpu_place_cruiser
+  # end
+  #
+  # def test_cpu_place_sub
+  #   refute nil, @game.cpu_place_sub
+  # end
+  # def test_cpu_place_sub2
+  #   refute nil, @game.cpu_place_sub
+  # end
+  # def test_cpu_place_sub3
+  #   refute nil, @game.cpu_place_sub
+  # end
+  # def test_cpu_place_sub4
+  #   refute nil, @game.cpu_place_sub
+  #end
+
+  def test_cpu_fire
+    assert_equal "M", @game.cpu_fire
   end
 
 end
