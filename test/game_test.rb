8c88c67bbfbc02306eed32f207126b1894533a03
PhilipDeFraties <philipjfry338@gmail.com>
2020-07-14 16:56:22 -0600

    Attempted to test user input for main_menu but will not work for loop.

--- test/game_test.rb
+++ test/game_test.rb
@@ -4,32 +4,24 @@ require './lib/ship'
 require './lib/cell'
 require './lib/board'
 require './lib/game'
+require 'mocha/minitest'
 require 'pry'
 
 class GameTest < Minitest::Test
 
-# def setup
-#   @game = Game.new
-#   @board = Board.new
-#   @board.generate_cells
-#   @cruiser = Ship.new("Cruiser", 3)
-#   @submarine = Ship.new("Submarine", 2)
-# end
-
-  # def test_it_exists
-  #   @game = Game.new
-  #
-  #   assert_instance_of Game, @game
-  # end
-
-def test_turn
+  def setup
     @game = Game.new
-    @board = @game.board
-    # @board.cells["A1"].fire_upon
-    @game.main_menu
-    binding.pry
-    @game.turn
-    assert_equal "You already fired at this spot", @game.turn
+  end
+
+  def test_it_exists
+
+    assert_instance_of Game, @game
+  end
+
+  def test_main_menu
+    @game.stubs(:get_user_input).returns("p")
+
+    assert_equal [], @game.main_menu
   end
 
 end
