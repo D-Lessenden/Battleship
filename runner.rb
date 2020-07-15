ca8a0c2a0cf807654e21747f4f2a393709b911aa
Daniel Lessenden <dless27@gmail.com>
2020-07-15 00:00:14 -0500

    Edit structure of runner

---
+++ runner.rb
@@ -0,0 +1,35 @@
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/game'
+
+
+@game = Game.new
+@game.game_play
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#add a legend, what does S H etc mean?
+
+
+#code solution that runs w/ emojis or with the normal method
+#terminal bash error
+
+
+
+#make text its own method and then call that method
+  #instead of clttering up turn and main_menu
+
+#every loop (if, while, until) can be its own method
